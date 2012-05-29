dd8521db18c266ba327ab412eb0f0f9d175b7bc0
Ryan Krauss <ryanlists@gmail.com>
2012-05-28 23:21:00 -0500

    picture_importer_pygtk v. 1.0.0 working

--- copy_based_on_EXIF_date.py
+++ copy_based_on_EXIF_date.py
@@ -1,4 +1,5 @@
-import photo_db, os, rwkos, shutil, re
+import photo_db, os, rwkos, shutil, re, glob, file_finder
+import time
 
 from file_finder import Image_Finder
 
@@ -8,24 +9,48 @@ def find_all_photos(pathin):
     return pathsout
 
 
-def build_path(photo, root='/mnt/personal/pictures/Joshua_Ryan', \
-               makedirs=True):
+def _build_path(year_str, month_str, day_str, \
+                root=None, makedirs=True):
+    if root is None:
+        root = '/mnt/personal/pictures/Joshua_Ryan/'
+        
+    year_folder = os.path.join(root, year_str)
+    month_folder_name = os.path.join(year_folder, month_str)
+    day_path = os.path.join(month_folder_name, day_str)
+    
+    if makedirs:
+        rwkos.make_dir(year_folder)
+        rwkos.make_dir(month_folder_name)
+        rwkos.make_dir(day_path)
+
+    return day_path
+
+
+def month_folder(month_int_str, year_str):
+    month_abbrev = photo_db.month_map[int(month_int_str)]
+    month_folder_name = '%s_%s' % (month_abbrev, year_str)
+    return month_folder_name
+
+
+def day_folder(day_str, month_int_str, year_str):
+    month_int = int(month_int_str)
+    day_int = int(day_str)
+    day_folder_name = '%s-%0.2i-%0.2i' % (year_str, month_int, day_int)
+    return day_folder_name
+
+
+def build_path(photo, root=None, makedirs=True):
     """Build the fullpath to the folder where photo should be moved
     to, i.e. root/year/month_year/YYYY-MM-DD.  If makedirs is True,
     then then make all the folders in the tree as needed."""
     year_str = '%i' % photo.year
-    year_folder = os.path.join(root, year_str)
     month_str = '%s_%s' % (photo.month, year_str)
-    month_folder = os.path.join(year_folder, month_str)
     month_num = photo_db.inverse_month_map[photo.month]
     day_str = '%s-%0.2i-%0.2i' % (year_str, month_num, photo.day)
-    day_path = os.path.join(month_folder, day_str)
     
-    if makedirs:
-        rwkos.make_dir(year_folder)
-        rwkos.make_dir(month_folder)
-        rwkos.make_dir(day_path)
-
+    day_path = _build_path(year_str, month_str, day_str, \
+                           root=root, \
+                           makedirs=makedirs)
     return day_path
 
 
@@ -48,11 +73,40 @@ def renumber_filename(namein, add_to=0, fmt='%0.6i'):
     return new_filename
 
 
-def copy_one_image(pathin):
+last_four_digits_pat = re.compile('(.*)([0-9]{4})')#insist on 4 digits at the end
+
+def separate_filename(pathin):
+    folder, filename = os.path.split(pathin)
+    fno, ext = os.path.splitext(filename)
+
+    q = last_four_digits_pat.search(fno)
+    return q.group(1), int(q.group(2))
+
+
+def find_file_number(pathin):
+    pre_str, mynum = separate_filename(pathin)
+    return mynum
+
+
+def find_max_and_min_file_numbers(folderpath):
+    """Assuming path contains picture and movie files that end in
+    digits, find the highest and lowest digit based on sorting the
+    paths."""
+    myfinder = file_finder.Multi_Media_Finder(curdir)
+    files = myfinder.Find_Top_Level_Files()
+    files.sort()
+    first_file = files[0]
+    last_file = files[-1]
+
+    low_num = find_file_number(first_file)
+    high_num = find_file_number(last_file)
+    return high_num, low_num
+    
+def copy_one_image(pathin, root=None):
     myphoto = photo_db.photo(pathin, get_EXIF_data=True, \
                              get_os_data=True, calc_md5=False, \
                              get_PIL_size=False, basepath=None, dictin=None)
-    destfolder = build_path(myphoto)
+    destfolder = build_path(myphoto, root=root)
     src_folder, name = os.path.split(myphoto.pathin)
     new_name = renumber_filename(name)
     destpath = os.path.join(destfolder, new_name)
@@ -60,9 +114,126 @@ def copy_one_image(pathin):
     return myphoto, destpath
 
 
+def _find_photo(pathin, inc=-1):
+    folder, filename = os.path.split(pathin)
+    part1, num_in = separate_filename(pathin)
+    
+    found = False
 
+    myfinder = file_finder.Multi_Media_Finder(folder)
+    files = myfinder.Find_Top_Level_Files()
+    files.sort()
+    N = len(files)
+    
+    index_in = files.index(pathin)
+
+    test_index = index_in + inc
+
+    exif_exts = ['.jpg','.nef','.cr2']
+    
+    while -1 < test_index < N:
+        test_path = files[test_index]
+        pne, ext = os.path.splitext(test_path)
+        if ext.lower() in exif_exts:
+            return test_path
+        else:
+            test_index += inc
+    
+    ## max_num, min_num = find_max_and_min_file_numbers(folder)
+    
+    ## while abs(i) < 1000:
+    ##     test_num = num_in + inc
+    ##     pat = part1 + str(test_num) + '.*'
+    ##     full_pat = os.path.join(folder, pat)
+    ##     matches = glob.glob(full_pat)
+    ##     if not matches:
+    ##         #glob returned an empty list; if a file was deleted this
+    ##         #might not mean anything.  How do we find the lowest
+    ##         #numbered file in a diretory?
+    ##         if (test_num < min_num) or (test_num > max_num):
+    ##             #we didn't find the next photo
+    ##             break
+
+def split_EXIF_date(date_str):
+    date, time = date_str.split(' ',1)
+    return date.strip()
+
+
+def get_EXIF_date(pathin):
+    myphoto = photo_db.photo(pathin, get_EXIF_data=True, \
+                             get_os_data=True, calc_md5=False, \
+                             get_PIL_size=False, basepath=None, dictin=None)    
+    return myphoto.exif_date
+
+
+def find_previous_photo(pathin):
+    return _find_photo(pathin, inc=-1)
+
+
+def find_next_photo(pathin):
+    return _find_photo(pathin, inc=1)
+
+
+def getctime_str(pathin):
+    sec1 = os.path.getctime(pathin)
+    struct1 = time.localtime(sec1)
+    str1 = time.strftime('%Y:%m:%d', struct1)
+    return str1
+
+
+def get_movie_date(pathin):
+    """determine the date associated with a movie file based on a
+    combination of the EXIF date of the next and previous still image
+    in the directory and the os.path.getctime of the movie file."""
+    prev_photo = find_previous_photo(pathin)
+    next_photo = find_next_photo(pathin)
+    prev_date = split_EXIF_date(get_EXIF_date(prev_photo))
+    next_date = split_EXIF_date(get_EXIF_date(next_photo))
+    ctime_str = getctime_str(pathin)
+    ## print('prev_date = ' + prev_date)
+    ## print('next_date = ' + next_date)
+    ## print('ctime_str = ' + ctime_str)
+
+    if prev_date == next_date:
+        return prev_date
+    elif (ctime_str == prev_date) or (ctime_str == next_date):
+        return ctime_str
+    else:
+        return None
+    
+
+def copy_one_movie_file(pathin, root=None):
+    """It is tricky to find the date for an movie file, because they
+    don't seem to work with EXIF.  My plan is to find the EXIF date of
+    the next and previous numbered still image file and compare them
+    to the os.getctime date.  If 2 of the 3 agree, use that date."""
+    date_str = get_movie_date(pathin)
+    assert date_str is not None, "did not find a valid date for " + pathin
+    year_str, month_str, day_str = date_str.split(':',2)
+    day_folder_name = day_folder(day_str, month_str, year_str)
+    month_folder_name = month_folder(month_str, year_str)
+    destfolder =_build_path(year_str, month_folder_name, day_folder_name, \
+                            root=root, makedirs=True)
+    src_folder, name = os.path.split(pathin)
+    new_name = renumber_filename(name)
+    destpath = os.path.join(destfolder, new_name)
+    ## print('--------------------')
+    ## print('pathin = ' + pathin)
+    ## print('destpath = ' + destpath)
+    shutil.move(pathin, destpath)
+    return destpath
+    
 if __name__ == '__main__':
-    mypath = '/home/ryan/Desktop/pics_2012/Apr_2012/unsorted'
-    myimages = find_all_photos(mypath)
-    for image in myimages:
-        photo, dp = copy_one_image(image)
+    ## mypath = '/home/ryan/Desktop/pics_2012/Apr_2012/unsorted'
+    ## myimages = find_all_photos(mypath)
+    ## for image in myimages:
+    ##     photo, dp = copy_one_image(image)
+
+    
+    mypath = '/media/NIKON D7000/DCIM/100D7000'
+    mov_pat = os.path.join(mypath, '*.MOV')
+    mov_files = glob.glob(mov_pat)
+    #for mov_file in mov_files[0:1]:
+    for mov_file in mov_files:
+        mydate = copy_one_movie_file(mov_file)
+    
--- picture_importer_pygtk.py
+++ picture_importer_pygtk.py
@@ -10,6 +10,66 @@ settings_path = 'picture_importer_settings.pkl'
 import pygtk
 pygtk.require('2.0')
 import gtk
+import gobject
+
+import file_finder
+
+import threading
+
+import copy_based_on_EXIF_date as copy_lib
+
+class copy_thread(threading.Thread):
+    def __init__(self, GUI, source_path, dest_root):
+        threading.Thread.__init__(self)
+        self.GUI = GUI
+        self.source_path = source_path
+        self.dest_root = dest_root
+        self.finder_all = file_finder.Multi_Media_Finder(source_path)
+        movie_exts = ['.mov', '.mp4', '.avi']
+        picture_exts = ['.jpg','.nef','.cr2']
+        self.movie_finder = file_finder.Multi_Media_Finder(source_path, \
+                                                           extlist=movie_exts)
+        self.picture_finder = file_finder.Multi_Media_Finder(source_path, \
+                                                             extlist=picture_exts)
+        self.all_paths = self.finder_all.Find_All_Files()
+        self.N = len(self.all_paths)
+
+        
+    def run(self):
+        #in order to fascilitate finding the dates of movie files
+        #based on neighboring stil pictures, you need to copy all
+        #movie files first
+
+        #but do you do this at the copy thread level or the GUI level?
+        self.movie_paths = self.movie_finder.Find_All_Files()
+        self.N_movies = len(self.movie_paths)
+        gobject.idle_add(self.GUI.append_to_log, 'Found %i movie files' % self.N_movies)
+                         
+        self.picture_paths = self.picture_finder.Find_All_Files()
+        self.N_pictures = len(self.picture_paths)
+        gobject.idle_add(self.GUI.append_to_log, 'Found %i picture files' % self.N_pictures)
+
+        if self.N_movies > 0:
+            gobject.idle_add(self.GUI.append_to_log, 'Copying movie files')
+        
+        for i, curpath in enumerate(self.movie_paths):
+            fraction = float(1.0+i)/self.N
+            folder, filename = os.path.split(curpath)
+            fno, ext = os.path.splitext(filename)
+            gobject.idle_add(self.GUI.set_progress, fraction, filename)
+            copy_lib.copy_one_movie_file(curpath, root=self.dest_root)
+
+        gobject.idle_add(self.GUI.append_to_log, 'Copying picture files')
+
+        for i, curpath in enumerate(self.picture_paths):
+            fraction = float(i+self.N_movies)/self.N
+            folder, filename = os.path.split(curpath)
+            fno, ext = os.path.splitext(filename)
+            gobject.idle_add(self.GUI.set_progress, fraction, filename)
+            copy_lib.copy_one_image(curpath, root=self.dest_root)
+            
+
+        gobject.idle_add(self.GUI.append_to_log, 'Copying completed.')
 
 
 def my_dir_chooser(pathin=None):
@@ -72,7 +132,54 @@ class picture_importer(rwkmisc.gui_that_saves):
         if path is not None:
             self.path2.set_text(path)
         #chooser.set_action(gtk.FILE_CHOOSER_ACTION_SELECT_FOLDER)
+
+
+    def set_progress(self, fraction, filename):
+        self.progress_bar.set_fraction(fraction)
+        self.progress_bar.set_text(filename)
         
+
+    def go(self, widget, data=None):
+        source_path = self.path1.get_text()
+        dest_root = self.path2.get_text()
+        if not source_path:
+            self.append_to_log('Cannot copy files without valid source path.')
+            return
+        if not os.path.exists(source_path):
+            self.append_to_log('source path does not exist')
+        if not os.path.exists(dest_root):
+            self.append_to_log('destination root does not exist:')
+            self.append_to_log(dest_root)
+
+        my_thread = copy_thread(self, source_path, dest_root)
+        my_thread.start()
+        
+    def append_to_log(self, msg, scroll=True):
+        if msg == '':
+            msg = '\n'
+        elif msg[-1] != '\n':
+            msg += '\n'
+        end_iter = self.textbuffer.get_end_iter()
+        self.textbuffer.insert(end_iter, msg)
+
+        if scroll:
+            #mark = self.textbuffer.get_insert()
+            #self.textview.scroll_to_mark(mark, 0.01)
+            #end_iter = self.textbuffer.get_end_iter()
+            #mymark = self.textbuffer.create_mark("mymark", end_iter, False)
+            #self.textview.scroll_to_mark(mymark, 0.45)
+            #self.textview.scroll_to_iter(end_iter, 0.01)
+            #self.textview.scroll_mark_onscreen(self.textbuffer.get_insert())
+            #self.textview.scroll_to_mark(self.textbuffer.get_insert(), 0)
+            adj = self.text_scrollw.get_vadjustment()
+            adj.set_value( adj.upper )# - adj.pagesize )
+
+
+    def clear_log(self):
+        start_iter = self.textbuffer.get_start_iter()
+        end_iter = self.textbuffer.get_end_iter()
+        self.textbuffer.delete(start_iter, end_iter)
+
 
     def delete_event(self, widget, event, data=None):
         # If you return FALSE in the "delete_event" signal handler,
@@ -129,7 +236,13 @@ class picture_importer(rwkmisc.gui_that_saves):
         # function is defined above.
         self.button2.connect("clicked", self.browse2, None)
 
-        
+
+        # Creates a new button with the label "Hello World".
+        self.go_button = gtk.Button("Copy files")
+        self.go_button.connect("clicked", self.go, None)        
+
+
+        #packing the gui boxes
         big_vbox = gtk.VBox(homogeneous=False)
 
         #source label
@@ -161,7 +274,74 @@ class picture_importer(rwkmisc.gui_that_saves):
         hbox4.pack_start(self.path2,False)
         hbox4.pack_start(self.button2,False)
         big_vbox.pack_start(hbox4,False)
+
+        #empty string spacer
+        spacer_label = gtk.Label('')
+        hbox5 = gtk.HBox(homogeneous=False)
+        hbox5.pack_start(spacer_label,False)
+        big_vbox.pack_start(hbox5,False)
+
+        #go button
+        hbox6 = gtk.HBox(homogeneous=False)
+        hbox6.pack_start(self.go_button,False)
+        big_vbox.pack_start(hbox6,False)
+
+
+        #empty string spacer
+        spacer_label = gtk.Label('')
+        hbox7 = gtk.HBox(homogeneous=False)
+        hbox7.pack_start(spacer_label,False)
+        big_vbox.pack_start(hbox7,False)
+
+        #progress label
+        progress_label = gtk.Label('Progress')
+        hbox8 = gtk.HBox(homogeneous=False)
+        hbox8.pack_start(progress_label,False)
+        big_vbox.pack_start(hbox8,False)
+
+
+        #progress bar
+        self.progress_bar = gtk.ProgressBar()
+        hbox9 = gtk.HBox(homogeneous=False)
+        hbox9.pack_start(self.progress_bar,False)
+        self.progress_bar.set_size_request(entry_width,25)
+        big_vbox.pack_start(hbox9,False)
+
         
+        #empty string spacer
+        spacer_label = gtk.Label('')
+        hbox10 = gtk.HBox(homogeneous=False)
+        hbox10.pack_start(spacer_label,False)
+        big_vbox.pack_start(hbox10,False)
+
+
+        #log label
+        log_label = gtk.Label('Log')
+        hbox11 = gtk.HBox(homogeneous=False)
+        hbox11.pack_start(log_label,False)
+        big_vbox.pack_start(hbox11,False, False, 5)
+
+        
+        #log text thingy
+        self.textview = gtk.TextView()
+        self.textbuffer = self.textview.get_buffer()
+
+        self.textbuffer.set_text("")
+
+        w = 500
+        h = 100
+        self.textview.set_size_request(w,h)
+        scrolled_window = gtk.ScrolledWindow()
+        scrolled_window.set_border_width(0)
+        big_vbox.pack_start(scrolled_window, False, False, 0)
+        scrolled_window.set_size_request(w,h)
+
+        scrolled_window.show()
+
+        scrolled_window.add_with_viewport(self.textview)
+        self.text_scrollw = scrolled_window
+        self.textview.show()
+
         # This will cause the window to be destroyed by calling
         # gtk_widget_destroy(window) when "clicked".  Again, the destroy
         # signal could come from here, or the window manager.
@@ -183,7 +363,16 @@ class picture_importer(rwkmisc.gui_that_saves):
 
         if os.path.exists(settings_path):
             self.load(settings_path)
-    
+
+
+        source_path = self.path1.get_text()
+        if source_path:
+            if os.path.exists(source_path):
+                myfinder = file_finder.Multi_Media_Finder(source_path)
+                files = myfinder.Find_Top_Level_Files()
+                self.append_to_log('Found %i image and movie files in %s.' % \
+                                   (len(files), source_path))
+                
         # and the window
         #self.window.show()
         self.window.set_title('Picture Importer v. 1.0.0')
@@ -209,6 +398,9 @@ class picture_importer(rwkmisc.gui_that_saves):
 #    - how many pictures or movies are in the source directory
 #    - if the source is a memory card, are there pictures in
 #      other folders that that would be reasonable to import?
+#
+#      - does the path start with /media?
+
 # 4. I need to handle the exif date of raw photos and movies,
 #    possibly using neiboring file #'s
 
@@ -219,4 +411,5 @@ class picture_importer(rwkmisc.gui_that_saves):
 # interpreter then create a HelloWorld instance and show it
 if __name__ == "__main__":
     myapp = picture_importer()
+    gtk.gdk.threads_init()
     myapp.main()
